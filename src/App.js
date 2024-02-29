7683546c883a289bbbdcc3b71e1d40e4eef0204f
sasank Prajapati <sasank45prajapati@gmail.com>
2024-03-01 00:52:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Menu from "./component/Menu";
+import Navbar from "./component/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+
+      <Navbar />
+      <Menu/>
+    </div>
+  );
+}
+
+export default App;
